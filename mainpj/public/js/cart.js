2e19bfe792da01d198cef54ccb723413070f8a93
marco.wong <s1993618@yahoo.com.hk>
2023-12-12 10:45:01 +0800

    want to fix total price

--- mainpj/public/js/cart.js
+++ mainpj/public/js/cart.js
@@ -4,7 +4,12 @@ async function inShoppingCart() {
   let shoppingCartRes = await fetch("/cart");
   let resp = await shoppingCartRes.json();
   let finalHTML = "";
+  
   for (let entry of resp) {
+    // let `${entry.quantity}` = x
+    // let `${entry.unit_price}` = y
+    // // let totalPrice = x*y
+    
     finalHTML += `
     <div class="row main align-items-center">
     <div class="col-2"><img class="img-fluid" src="/picture/${entry.image}"></div>
@@ -16,7 +21,7 @@ async function inShoppingCart() {
       <!-- <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> -->
       Qantity:${entry.quantity}
     </div>
-    <div class="col">$${entry.unit_price}<span class="close">&#10005;</span></div>
+    <div class="col">123<span class="close">&#10005;</span></div>
   </div>`
   }
   document.querySelector(".product-area").innerHTML = finalHTML;
