6f65e31e3009a3b9e288e67dcd8c86df496f0af9
marco.wong <s1993618@yahoo.com.hk>
2023-12-13 09:51:27 +0800

    casual css style

--- mainpj/public/js/all.js
+++ mainpj/public/js/all.js
@@ -4,7 +4,7 @@ document.querySelector(
   ".first"
 ).innerHTML = `<nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">鬼滅之刃傭</a>
+      <a class="navbar-brand" href="https://youtu.be/L3z6rcD_LQ8?t=22">鬼滅之刃傭</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -14,9 +14,7 @@ document.querySelector(
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="/">HomePage</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="contact_us.html">Contact us</a>
-          </li>
+   
           <!-- <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
             </li> -->
@@ -33,9 +31,7 @@ document.querySelector(
             </ul>
           </li>
         </ul>
-        <form>
-          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-        </form>
+
         <a class="bi bi-person-fill" href="login.html"></a>
 
         <a class="bi bi-cart2" href="cart.html"></a>
