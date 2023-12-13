ea089270fc4dd053f7a299af52ce91c53b51c548
Jozchow8 <mryiunamchow@gmail.com>
2023-12-13 11:11:55 +0800

    get final version

--- mainpj/app.ts
+++ mainpj/app.ts
@@ -107,7 +107,13 @@ app.post("/register", async (req: Request, res: Response) => {
       let hashed = await hashPassword(req.body.passwordInput1);
       await pgClient.query(
         "INSERT INTO users (first_name,last_name,email,password,contact_number) VALUES ($1,$2,$3,$4,$5)",
-        [req.body.first_name,req.body.last_name,req.body.email, hashed,req.body.contact_number]
+        [
+          req.body.first_name,
+          req.body.last_name,
+          req.body.email,
+          hashed,
+          req.body.contact_number,
+        ]
       );
       console.log("app.ts 83");
       res.json({ message: "register success" });
@@ -212,25 +218,108 @@ app.get("/cart", async (req, res) => {
   console.log("$$$$$$$$$$$$$$$", result.rows);
   res.json(result.rows);
 });
-app.post("/thankyou", async (req, res) => {
-  console.log(req.body)
+
+//////////////////////////////////thank you///////////////////////////////////////
+
+app.post("/thankyou", isLoggedIn, async (req, res) => {
+  console.log(req.body, req.session?.email);
+
+  // need to check stock count!!! use if ,else bouns
+
   await pgClient.query(
-    "INSERT INTO receipts (total,quantity,user_id) VALUES ($1,$2,(SELECT id from users where email = $3)",
-    [req.body.total, req.body.quantity, req.session.email]
- 
+    //   "INSERT INTO receipts (total,quantity,user_id) VALUES ($1,$2,(SELECT id from users where email = $3)",
+    //   [req.body.total, req.body.quantity, req.session.email]
+
+    // );
+    "INSERT INTO carts (product_option_id,user_id,quantity,total) VALUES  ($1,(SELECT id from users where email = $2),$3,$4)",
+    [
+      req.body.product_option_id,
+      req.session.email,
+      req.body.quantity,
+      req.body.total,
+    ]
   );
-  res.json({ message: "added to receipt" });
+
+  res.json({ message: "added receipt YEAH!!!!!" });
 });
 
-// app.get("/thankyou", async (req, res) => {
-//   let result = await pgClient.query(
-//     "select * from receipts join users on user_id = users.id join carts on users.id = carts.user_id join product_options on product_option_id = product_options.id;"
-//   );
-// });
+app.get("/thankyou", async (req, res) => {
+  let result = await pgClient.query(
+    //     "SELECT * FROM receipts JOIN users ON user_id = users.id JOIN carts ON users.id = carts.user_id JOIN product_options ON product_option_id = product_options.idwhere user_id = (SELECT id from users where email = $1)"
 
-// app.get("/thankyou",async(req,res)=>{
-//   let result = await pgClient.query("SELECT * FROM receipts JOIN users on user_id = users.id join carts on users.id = carts.id")
-// })
+    //   );
+    "SELECT * FROM carts join product_options on product_option_id = product_options.id join products on product_id = products.id where user_id = (SELECT id from users where email = $1)",
+    [req.session.email]
+  );
+  console.log("payment susscesssss!!!!!!", result.rows);
+  res.json(result.rows);
+});
+
+app.get("/payment", async (req: express.Request, res: express.Response) => {
+  const email = req.session.email;
+  const userId = await pgClient.query(
+    `
+    SELECT id
+    FROM users
+    WHERE email = $1
+  `,
+    [email]
+  );
+  const getCartProductQuery = `
+    SELECT *
+    FROM carts
+    WHERE user_id = $1
+    )
+  `;
+  const removeCartProductQuery = `
+      DELETE
+      FROM carts
+      WEHRE user_id = $1
+      )
+  `;
+  const addTransactionsQuery = `
+        INSERT INTO receipts
+        (total, quanitity, user_id, stripe_id)
+        VALUES
+        ($1, $2, $3, $4)
+  `;
+  const addTransactionDetailsQuery = `
+  `;
+  const cartProducts = await pgClient.query(getCartProductQuery, [userId]);
+  const allCartProductInfo = await pgClient.query(`
+    SELECT 
+      c.user_id as user_id,
+      c.product_option_id as product_option_id,
+      c.quantity as quantity,
+      '1234' as stripe_id,
+      p.unit_price as unit_price,
+    FROM carts c
+    JOIN product_options po ON c.product_option_id = po.id
+    JOIN products p ON po.product_id = p.id
+  `);
+  const total = allCartProductInfo.rows.reduce(
+    (acc, cur) => acc + cur.unit_price * cur.quantity,
+    0
+  );
+  const quanitity = allCartProductInfo.rows.reduce(
+    (acc, cur) => acc + cur.quantity,
+    0
+  );
+  const addTransaction = await pgClient.query(addTransactionsQuery, [
+    total,
+    quanitity,
+    userId,
+    1234,
+  ]);
+  console.log(
+    removeCartProductQuery,
+    addTransactionDetailsQuery,
+    cartProducts,
+    addTransaction
+  );
+});
+
+/////////////////////////////////////////////////////////////////////////
 
 // identifier
 app.use(express.static("public"));
--- mainpj/public/js/thankyou.js
+++ mainpj/public/js/thankyou.js
@@ -1,6 +1,3 @@
-
-
-
 // console.log("this is thankyou js")
 // addToReceipt()
 // async function addToReceipt(){
@@ -23,4 +20,84 @@
 //             total:
 //         })
 //     }
+// }
+
+inShoppingCart();
+
+
+
+async function inShoppingCart() {
+    
+  let shoppingCartRes = await fetch("/thankyou");
+  let resp = await shoppingCartRes.json();
+  console.log(resp);
+  let finalHTML = "";
+
+  for (let entry of resp) {
+    const x = `${entry.quantity}`;
+    const y = `${entry.unit_price}`;
+    const totalPrice = x * y;
+
+    finalHTML += `
+    <div class="row main align-items-center">
+    
+    <div class="col">
+      <div class="row text-muted">${entry.name}</div>
+     
+    </div>
+    <div class="col">
+      <!-- <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> -->
+      Quantity:${entry.quantity}
+    </div>
+    <div class="ttlpce">Price : $${totalPrice}</div>
+    
+
+    
+  </div>`;
+  }
+  document.querySelector(".product-area").innerHTML = finalHTML;
+
+  const totalPrice = resp.reduce((acc, entry) => {
+    return (acc += entry.unit_price * entry.quantity);
+  }, 0);
+  // console.log(totalPrice);
+  let summaryHTML = "";
+  summaryHTML += `<div class="col totalPrice">Total Price :$${totalPrice}</div>
+  `;
+
+  document.querySelector(".totalPrice").innerHTML = summaryHTML;
+
+  const totalQuantity = resp.reduce((acc, entry) => {
+    return (acc += entry.quantity);
+  }, 0);
+  console.log(totalQuantity);
+  let totalQuantityItem = "";
+  totalQuantityItem += `<div>Total ${totalQuantity} Items<div>`;
+  document.querySelector(".totalItem").innerHTML = totalQuantityItem;
+
+
+let oLiReceiptHTML = "";
+oLiReceiptHTML += `<div class="col"><h4><b>Order List Receipt #$(entry.receipt.id)</b></h4></div>
+`;
+
+document.querySelector(".oLiRece").innerHTML = oLiReceiptHTML;
+
+
+let userInPutHTML = "";
+userInPutHTML += `<div class="col-2">$(entry.user_name)</div>
+`;
+
+document.querySelector(".userInPut").innerHTML = userInPutHTML;
+}
+
+// document.querySelector('button.payment').addEventListener('click', makePayment)
+
+// async function makePayment(e){
+//     e.preventDefault()
+//     const httpResp = await fetch('/thankyou')
+//     const resp = await httpResp.json()
+//     if (resp.message == 'ok'){
+
+//     }
+
 // }
